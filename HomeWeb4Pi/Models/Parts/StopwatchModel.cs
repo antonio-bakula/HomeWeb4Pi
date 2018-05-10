01e5f06b41ebaa13f01ab9ba168025d2d7829019
Antonio Bakula <antonio@wem.hr>
2018-05-10 23:50:25 +0200

    Stopwatc

--- HomeWeb4Pi/Models/Parts/StopwatchModel.cs
+++ HomeWeb4Pi/Models/Parts/StopwatchModel.cs
@@ -7,5 +7,24 @@ namespace HomeWeb4Pi.Models.Parts
 {
   public class StopwatchModel
   {
+    public List<StopwatchPresetItemModel> Presets { get; private set; }
+
+    public StopwatchModel()
+    {
+      var durations = new List<int> { 5, 10, 15, 30, 45, 1 };
+      this.Presets = durations.Select(ii => new StopwatchPresetItemModel(ii)).ToList();
+    }
+  }
+
+  public class StopwatchPresetItemModel
+  {
+    public string Minutes { get; private set; }
+    public string Period { get; set; }
+
+    public StopwatchPresetItemModel(int presetDuration)
+    {
+      this.Minutes = presetDuration.ToString();
+      this.Period = presetDuration.ToString("00") + ":00";
+    }
   }
 }
